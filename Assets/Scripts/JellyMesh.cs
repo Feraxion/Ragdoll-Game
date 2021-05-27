5006f1607da03bed3562bb380a9c2df3671beb41
NagataKyoshi <Nagatakyoshii@gmail.com>
2021-05-27 17:10:24 +0300

    JellyMesh

---
+++ Assets/Scripts/JellyMesh.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JellyMesh : MonoBehaviour
+{
+    public float Intensity = 1f;
+    public float mass = 1f;
+    public float stifness = 1f;
+    public float damping = 0.75f;
+    private Mesh originalMesh;
+    private Mesh meshClone;
+    private MeshRenderer renderer;
+
+    private JellyVertex[] jellyV;
+    private Vector3[] vertexArray;
+    void Start()
+    {
+        originalMesh = GetComponent<MeshFilter>().sharedMesh;
+        meshClone = Instantiate(originalMesh);
+        GetComponent<MeshFilter>().sharedMesh = meshClone;
+        renderer = GetComponent<MeshRenderer>();
+
+        jellyV = new JellyVertex[meshClone.vertices.Length];
+        for (int i = 0; i < meshClone.vertices.Length; i++)
+        {
+            jellyV[i] = new JellyVertex(i, transform.TransformPoint(meshClone.vertices[i]));
+        }
+
+    }
+
+   
+    void FixedUpdate()
+    {
+        vertexArray = originalMesh.vertices;
+        for (int i = 0; i < jellyV.Length; i++)
+        {
+            Vector3 target = transform.TransformPoint(vertexArray[jellyV[i].ID]);
+            float intensity = (1 - (renderer.bounds.max.y - target.y) / renderer.bounds.size.y) * Intensity;
+            jellyV[i].Shake(target, mass, stifness, damping);
+            target = transform.InverseTransformPoint(jellyV[i].Position);
+            vertexArray[jellyV[i].ID] = Vector3.Lerp(vertexArray[jellyV[i].ID], target, intensity);
+        }
+        meshClone.vertices = vertexArray;
+    }
+}
---
+++ Assets/Scripts/JellyVertex.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JellyVertex
+{
+    public int ID;
+    public Vector3 Position;
+    public Vector3 velocity, Force;
+    public JellyVertex(int _id, Vector3 _pos)
+    {
+        ID = _id;
+        Position = _pos;
+    }
+
+    public void Shake(Vector3 target, float m, float s, float d)
+    {
+        Force = (target - Position) * s;
+        velocity = (velocity + Force / m) * d;
+        Position += velocity;
+        if ((velocity + Force + Force / m ).magnitude < 0.001f )
+        {
+            Position = target;
+        }
+    }
+}
