68d92c26674ee3ac2c089269938bc43cd09e3679
Suleyman Enes Yavuz <enesy375@gmail.com>
2021-05-31 10:28:32 +0300

    Tries

---
+++ Assets/Scripts/HoldFloat.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoldFloat : MonoBehaviour
+{
+    private Vector3 positionn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        positionn = gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = positionn;
+    }
+}
